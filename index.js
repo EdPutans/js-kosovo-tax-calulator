bf1ad21c20db21c410e0d75db4f5184e97435544
Ed 'Catboi' Putans <edputans@gmail.com>
2022-05-13 16:05:49 +0100

    created template

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const salary = 500;
-
-const contributions = salary * 0.05;
-
-const taxableAmount = salary - contributions;
-
-const taxOver450 = (taxableAmount - 450) * 0.10;
-const taxOver250 = Math.min(450 - 250, taxableAmount - 250) * 0.08;
-const taxOver80 = Math.min(250 - 80, taxableAmount - 80) * 0.04;
-
-const taxUnder80 = 0;
-
-const totalTaxes =
-  Math.max(0, taxOver450)
-  + Math.max(0, taxOver250)
-  + Math.max(0, taxOver80)
-  + Math.max(0, taxUnder80);
-
-const finalResult = taxableAmount - totalTaxes;
-
-console.log(
-  `On a monthly wage of ${salary} EUR a Kosovian would contribute ${contributions} EUR towards their pension, pay a total of ${totalTaxes} EUR of tax and receive a net of ${finalResult} EUR`
-)
+/**
+ * Based on a monthly Gross wage of 500 EUR ,
+ * contributions towards pension of 5%, a Kosovian would pay:
+ * 25EUR towards their pension
+ * 6.80EUR for the 80-250EUR part of their taxable income
+ * 16.00 EUR for the 250-450EUR part
+ * and 2.50EUR for the over 450EUR part
+ * 
+ * Their final take-home pay is 449.70 EUR
+ * 
+ * You can test different values here:
+ * https://www.rroga.com/en/llogaritja-e-rroges
+ */
